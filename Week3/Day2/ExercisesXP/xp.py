ab4bc64791af1757e8fe40a549b2b0be634ca9ac
aviKohane <kohaneavi@gmail.com>
2024-03-11 17:06:43 +0200

    ex3

--- Week3/Day2/ExercisesXP/xp.py
+++ Week3/Day2/ExercisesXP/xp.py
@@ -95,18 +95,3 @@ print(dog3.bark())
 print(dog2.run_speed())
 print(dog1.fight(dog2))      
 
-# 🌟 Exercise 3 : Dogs Domesticated
-# Instructions
-# Create a new python file and import your Dog class from the previous exercise.
-# In the new python file, create a class named PetDog that inherits from Dog.
-# Add an attribute called trained to the __init__ method, this attribute is a boolean and the value should be False by default.
-# Add the following methods:
-# train: prints the output of bark and switches the trained boolean to True
-
-# play: takes a parameter which value is a few names of other Dog instances (use *args). The method should print the following string: “dog_names all play together”.
-
-# do_a_trick: If the dog is trained the method should print one of the following sentences at random:
-# “dog_name does a barrel roll”.
-# “dog_name stands on his back legs”.
-# “dog_name shakes your hand”.
-# “dog_name plays dead”.
